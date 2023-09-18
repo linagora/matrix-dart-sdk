c730ba1af0d27465315db0e22351aff0be0cf62a
MinhDV <drminh2807@gmail.com>
2023-09-18 15:40:46 +0700

    fix: Search event not return when result not found

--- lib/src/timeline.dart
+++ lib/src/timeline.dart
@@ -40,7 +40,6 @@ class Timeline {
   final void Function(int index)? onInsert;
   final void Function(int index)? onRemove;
   final void Function()? onNewEvent;
-
   StreamSubscription<Event>? timelineSub;
   StreamSubscription<Event>? historySub;
   StreamSubscription<SyncUpdate>? roomSub;
@@ -679,9 +678,15 @@ class Timeline {
 
     // Search on the server
     prevBatch ??= room.prev_batch;
+    var ignoreEventIds = <String>[];
     if (sinceEventId != null) {
-      prevBatch =
-          (await room.client.getEventContext(room.id, sinceEventId)).end;
+      final eventContext =
+          await room.client.getEventContext(room.id, sinceEventId);
+      final eventIdsAfter =
+          eventContext.eventsAfter?.map((event) => event.eventId).toList() ??
+              [];
+      ignoreEventIds = eventIdsAfter..add(sinceEventId);
+      prevBatch = eventContext.end;
     }
     final encryption = room.client.encryption;
     for (var i = 0; i < maxHistoryRequests; i++) {
@@ -697,6 +702,9 @@ class Timeline {
         );
         for (final matrixEvent in resp.chunk) {
           var event = Event.fromMatrixEvent(matrixEvent, room);
+          if (ignoreEventIds.contains(event.eventId)) {
+            continue;
+          }
           if (event.type == EventTypes.Encrypted && encryption != null) {
             event = await encryption.decryptRoomEvent(event);
             if (event.type == EventTypes.Encrypted &&
@@ -722,7 +730,7 @@ class Timeline {
         rethrow;
       }
     }
-    return;
+    yield (found, null);
   }
 }
 
