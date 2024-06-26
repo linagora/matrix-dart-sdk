0f9721a323746160be9a2c7646ef5339ff0352bd
HuyNguyen <quanghnguyen@linagora.com>
2024-06-26 17:18:43 +0700

    Update date time when use `sendEvent()`

--- lib/src/room.dart
+++ lib/src/room.dart
@@ -1153,6 +1153,7 @@ class Room {
     String? editEventId,
     String? threadRootEventId,
     String? threadLastEventId,
+    DateTime? sentDate,
   }) async {
     // Create new transaction id
     final String messageID;
@@ -1229,7 +1230,6 @@ class Room {
         content['formatted_body'] = '* ${content['formatted_body']}';
       }
     }
-    final sentDate = DateTime.now();
     final syncUpdate = SyncUpdate(
       nextBatch: '',
       rooms: RoomsUpdate(
@@ -1242,7 +1242,7 @@ class Room {
                   type: type,
                   eventId: messageID,
                   senderId: client.userID!,
-                  originServerTs: sentDate,
+                  originServerTs: sentDate ?? DateTime.now(),
                   unsigned: {
                     messageSendingStatusKey: EventStatus.sending.intValue,
                     'transaction_id': messageID,
