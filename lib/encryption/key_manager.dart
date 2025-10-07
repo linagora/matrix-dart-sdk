bb899528aa65f64ef7d69e1b0c3f8bbc03151725
Dang Dat <tddang@linagora.com>
2025-10-07 11:17:22 +0700

    fixup! Remove olm dependency

--- lib/encryption/key_manager.dart
+++ lib/encryption/key_manager.dart
@@ -843,6 +843,9 @@ class KeyManager {
 
     Future<void> uploadInternal() async {
       try {
+        if (!vod.isInitialized()) {
+          await vod.init();
+        }
         await client.userDeviceKeysLoading;
 
         if (!(await isCached())) {
